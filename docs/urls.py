179b99cc5f13664a608dbc51677cefaf1ab3d75c
AdrianGustafson <Adrian@Adrians-MacBook-Pro-2.local>
2016-10-16 14:53:37 +0200

    Added the docs app
    Added the skeleton for the docs app and a static folder for index.html
    landing page

---
+++ docs/urls.py
@@ -0,0 +1,12 @@
+'''
+Created on 16 okt. 2016
+
+@author: Adrian
+'''
+from django.conf.urls import url
+
+app_name = 'docs'
+urlpatterns = [
+    
+    #url(r'^$', IndexView.as_view(), name = 'index'),
+]
